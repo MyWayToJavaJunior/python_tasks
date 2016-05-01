9c1dc4b0fd7f7b0ec994ed90372d4504940366b7
Safin Timur <timurtlt96@mail.ru>
2016-05-01 17:30:48 +0300

    create task cat_words.py

---
+++ tasks/regxep/cat_words.py
@@ -0,0 +1,10 @@
+import sys
+import re
+
+pattern = r'.*?\bcat\b.*?'
+string = ['cat', 'catapult and cat', 'catac', 'concat', 'Cat', '"cat"', '!cat?']
+
+for line in string:
+    line = line.rstrip()
+    if re.match(pattern, line):
+        print(line)
