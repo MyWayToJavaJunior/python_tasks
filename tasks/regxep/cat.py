be1a09c96bff8453014b788ea74898df042d84c2
Safin Timur <timurtlt96@mail.ru>
2016-05-01 17:03:52 +0300

    create task cat.py

---
+++ tasks/regxep/cat.py
@@ -0,0 +1,10 @@
+import sys
+import re
+
+pattern = r'cat'
+string = ['catcat', 'cat and cat', 'catac', 'cat', 'ccaatt']
+for line in string:
+    line = line.rstrip()
+    r = re.findall(pattern, line)
+    if len(r) > 1:
+        print(line)
